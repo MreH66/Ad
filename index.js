82b03ee342645e55fe24b0dbfbe5738ca64b60af
Marko G <gvozdenovmarko@gmail.com>
2023-02-23 11:39:19 +0100

    Initical Commit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    
-  </body>
-</html>
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const https = require("https");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("/publick"));
+app.use("/publick", express.static(__dirname + "/publick"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`);
+});
